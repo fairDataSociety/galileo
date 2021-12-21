0665ae3bfa21c00564927cd078ad66499bb3c0ed
Crt Ahlin <crt.ahlin@gmail.com>
2021-12-21 13:33:00 +0100

    update about

--- web/src/About.js
+++ web/src/About.js
@@ -4,5 +4,13 @@ export default function About() {
             Galileo - an open source project that allows you to use, create and modify maps of various
             participants.
         </p>
+        <p>
+            Find more information on how to contribute maps in
+            <a href="https://github.com/fairDataSociety/Galileo">Github & Docs</a>.
+        </p>
+        <p>
+            Join the community on
+            <a href="https://discord.gg/KrVTmahcUA">Fair Data Society Discord</a>.
+        </p>
     </div>;
 }
